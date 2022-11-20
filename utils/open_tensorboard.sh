51fa0e0db30f1c4d5e51a6861feaa7774adebae4
isZXY <61372482+isZXY@users.noreply.github.com>
2022-11-20 15:14:34 +0800

    add utils: to start a tensorboard from server to local

---
+++ utils/open_tensorboard.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ssh -L 16007:localhost:6006 -p 8004 xyzhu@10.103.10.151 <<remotessh
+    conda activate RL
+    cd RL/DI-Adventure
+    tensorboard --logdir mario_dqn/exp/mario_dqn_baseline_seed0_221119_173908 --bind_all --port=6006
+    exit
+remotessh
